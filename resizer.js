a1783da53bbf5e6de919864f9e0201648ec8a650
Erfi Anugrah <erfi.anugrah@gmail.com>
2021-04-22 11:08:52 +0800

    removed url from new request

--- resizer.js
+++ resizer.js
@@ -32,7 +32,7 @@ const imageDeviceResized = imageDevice.find( ({asset}) => device == asset)
 const imageURLResized = imageURL.find( ({asset}) => urlParams.match(asset))
 const image = cache ? imageDeviceResized : imageURLResized
 
-const subRequest = new Request(request.url)
+const subRequest = new Request(request)
 return await fetch(subRequest,
         { cf:
             {
