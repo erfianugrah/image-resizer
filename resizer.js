a48297effb25bdde3b094fef1cdb99a68979a99e
Erfi Anugrah <erfi.anugrah@gmail.com>
2021-04-22 11:41:31 +0800

    Resetting

--- resizer.js
+++ resizer.js
@@ -1,18 +1,9 @@
-/*addEventListener('fetch', event => {
-    if (/image-resizing/.test(event.request.headers.get("via"))) {
-        return 
-    }
-    event.respondWith(handleRequest(event.request))
-})
-*/
-
 addEventListener('fetch', event => {
-    if (event.request.headers.has("via") &&
-        event.request.headers.get("via").lastIndexOf("image-resizing-proxy")>0) {
-            return
+    if (/image-resizing/.test(event.request.headers.get("via"))) {
+        return event.respondWith(handleRequest(event.request))
     }
-    event.respondWith(handleRequest(event.request))
 })
+
 async function handleRequest(request) {
 
 const newRequest = new URL(request.url)
