855be11ce99356170323b9c09718fc606b1007d1
Erfi Anugrah <erfi.anugrah@gmail.com>
2021-04-23 16:58:47 +0800

    Added response.redirected

--- resizer.js
+++ resizer.js
@@ -67,6 +67,6 @@ const newResponse = await fetch(subRequest,
         })
 
 const response = new Response(newResponse.body, newResponse)
-const catchResponseError = response.ok ? response : await fetch(newRequest)
+const catchResponseError = response.ok || response.redirected ? response : await fetch(newRequest)
 return catchResponseError
 }
