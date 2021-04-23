7da8a749ff602de0a7476316e5cab2ea3d9b2d78
Erfi Anugrah <erfi.anugrah@gmail.com>
2021-04-23 18:38:40 +0800

    response headers

--- resizer.js
+++ resizer.js
@@ -67,7 +67,10 @@ const newResponse = await fetch(subRequest,
         })
 
 let response = new Response(newResponse.body, newResponse)
-response.headers.set("debug",JSON.stringify(image))
+response.headers.set("debug", JSON.stringify(image))
+response.headers.set("imageDeviceResized", console.log(imageDeviceResized))
+response.headers.set("imageDeviceResized", console.log(imageURLResized))
+
 const catchResponseError = response.ok ? response : await fetch(newRequest)
 return catchResponseError
 }
