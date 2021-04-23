f257326fc47f007f636760b94c27b572e012c6c8
Erfi Anugrah <erfi.anugrah@gmail.com>
2021-04-23 15:43:45 +0800

    it fucking works

--- resizer.js
+++ resizer.js
@@ -43,7 +43,7 @@ const cache = cacheAssets_match ? cacheAssets_match : ''
 
 const imageDeviceResized = imageDevice.find( ({asset}) => device == asset)
 const imageURLResized = imageURL.find( ({asset}) => urlParams.get(asset))
-const image = cache ? imageURLResized : ''
+const image = cache ? imageURLResized || imageDeviceResized : ''
 
 const newResponse = await fetch(subRequest,
         { cf:
