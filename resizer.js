5360510af5d4d39d2615840f9ff14236441287a3
Erfi Anugrah <erfi.anugrah@gmail.com>
2021-07-01 12:48:03 +0800

    test format auto v6

--- resizer.js
+++ resizer.js
@@ -16,8 +16,8 @@ const cacheAssets = [
 
 const imageDeviceOptions = {
     desktop: { height: 1440, width: 2560, fit: 'scale-down', metadata: 'copyright', quality: 85, format: 'auto'},
-    tablet: { height: 1080, width: 1920, fit: 'scale-down', metadata: 'copyright', quality: 85 },
-    mobile: { height: 720, width: 1280, fit: 'scale-down', metadata: 'copyright', quality: 85 }
+    tablet: { height: 1080, width: 1920, fit: 'scale-down', metadata: 'copyright', quality: 85, format: 'auto'},
+    mobile: { height: 720, width: 1280, fit: 'scale-down', metadata: 'copyright', quality: 85, format: 'auto'}
 }
 
 const height = urlParams.get('height') || undefined
