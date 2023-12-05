4b6d3c749c76d4a8f0239925d44a559e9acbd9d2
Erfi Anugrah <erfi.anugrah@gmail.com>
2023-12-05 16:42:34 +0000

    set undici so that it won't resize when calling from the page build

--- resizer.mjs
+++ resizer.mjs
@@ -1,6 +1,6 @@
 export default {
     fetch: (request, env, ctx) => {
-        if (!/image-resizing/.test(request.headers.get("via"))) {
+        if (!/image-resizing/.test(request.headers.get("via")) && !/undici/.test(request.headers.get("user-agent"))) {
             return resizer(request);
         }
         return fetch(request); // pass-through and continue
@@ -56,66 +56,64 @@ async function resizer(request) {
         imageResizer.format = 'webp'
     }
 
-    if (request.headers.get("User-Agent").toLowerCase() !== "undici") {
-        let newResponse = await fetch(request,
+    let newResponse = await fetch(request,
+        {
+            headers: {
+                "cf-feat-tiered-cache": "image"
+            },
+            cf:
             {
-                headers: {
-                    "cf-feat-tiered-cache": "image"
+                polish: "off",
+                mirage: false,
+                // cacheKey: cache.key,
+                cacheEverything: true,
+                cacheTtl: cache.ok,
+                // cacheTtlByStatus: {
+                //     '100-199': cache.info,
+                //     '200-299': cache.ok,
+                //     '300-399': cache.redirects,
+                //     '400-499': cache.clientError,
+                //     '500-599': cache.serverError
+                // },
+                image: {
+                    width: imageResizer.width,
+                    height: imageResizer.height,
+                    fit: imageResizer.fit,
+                    metadata: imageResizer.metadata,
+                    quality: imageResizer.quality,
+                    format: imageResizer.format
                 },
-                cf:
-                {
-                    polish: "off",
-                    mirage: false,
-                    // cacheKey: cache.key,
-                    cacheEverything: true,
-                    cacheTtl: cache.ok,
-                    // cacheTtlByStatus: {
-                    //     '100-199': cache.info,
-                    //     '200-299': cache.ok,
-                    //     '300-399': cache.redirects,
-                    //     '400-499': cache.clientError,
-                    //     '500-599': cache.serverError
-                    // },
-                    image: {
-                        width: imageResizer.width,
-                        height: imageResizer.height,
-                        fit: imageResizer.fit,
-                        metadata: imageResizer.metadata,
-                        quality: imageResizer.quality,
-                        format: imageResizer.format
-                    },
-                    cacheTags: [
-                        "image"
-                    ]
-                },
-            })
+                cacheTags: [
+                    "image"
+                ]
+            },
+        })
 
-        let response = new Response(newResponse.body, newResponse)
+    let response = new Response(newResponse.body, newResponse)
 
-        let cacheControl = '';
+    let cacheControl = '';
 
-        // Find the matching asset in the cacheAssets array
-        let matchedAsset = cacheAssets.find(asset => asset.regex.test(newURL));
+    // Find the matching asset in the cacheAssets array
+    let matchedAsset = cacheAssets.find(asset => asset.regex.test(newURL));
 
-        if (matchedAsset) {
-            // Set the cache-control header based on the asset type
-            if (response.status >= 200 && response.status < 300) {
-                cacheControl = `public, max-age=${matchedAsset.ok}`;
-            } else if (response.status >= 300 && response.status < 400) {
-                cacheControl = `public, max-age=${matchedAsset.redirects}`;
-            } else if (response.status >= 400 && response.status < 500) {
-                cacheControl = `public, max-age=${matchedAsset.clientError}`;
-            } else if (response.status >= 500 && response.status < 600) {
-                cacheControl = `public, max-age=${matchedAsset.serverError}`;
-            }
+    if (matchedAsset) {
+        // Set the cache-control header based on the asset type
+        if (response.status >= 200 && response.status < 300) {
+            cacheControl = `public, max-age=${matchedAsset.ok}`;
+        } else if (response.status >= 300 && response.status < 400) {
+            cacheControl = `public, max-age=${matchedAsset.redirects}`;
+        } else if (response.status >= 400 && response.status < 500) {
+            cacheControl = `public, max-age=${matchedAsset.clientError}`;
+        } else if (response.status >= 500 && response.status < 600) {
+            cacheControl = `public, max-age=${matchedAsset.serverError}`;
         }
-
-        // Set the cache-control header on the response
-        response.headers.set('Cache-Control', cacheControl);
-        response.headers.set("debug-ir", JSON.stringify(imageResizer))
-        response.headers.set("debug-cache", JSON.stringify(cache))
-
-        const catchResponseError = response.ok || response.redirected ? response : await fetch(request)
-        return catchResponseError
     }
+
+    // Set the cache-control header on the response
+    response.headers.set('Cache-Control', cacheControl);
+    response.headers.set("debug-ir", JSON.stringify(imageResizer))
+    response.headers.set("debug-cache", JSON.stringify(cache))
+
+    const catchResponseError = response.ok || response.redirected ? response : await fetch(request)
+    return catchResponseError
 }
