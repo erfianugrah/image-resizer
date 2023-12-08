faf76d54851fd49e51c9f0a493ace0352b816fd1
Erfi Anugrah <erfi.anugrah@gmail.com>
2023-12-08 14:42:57 +0000

    add JPG

--- resizer.mjs
+++ resizer.mjs
@@ -14,7 +14,7 @@ async function resizer(request) {
     const urlParams = newRequest.searchParams
 
     const cacheAssets = [
-        { asset: 'image', /*key: customCacheKey,*/ regex: /^.*\.(jpe?g|png|gif|webp|svg)/, info: 0, ok: 31536000, redirects: 31536000, clientError: 10, serverError: 1 },
+        { asset: 'image', /*key: customCacheKey,*/ regex: /^.*\.(jpe?g|JPG|png|gif|webp|svg)/, info: 0, ok: 31536000, redirects: 31536000, clientError: 10, serverError: 1 },
     ]
 
     const imageDeviceOptions = {
